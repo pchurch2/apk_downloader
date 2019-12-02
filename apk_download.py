dcb30e49851a8bcf52fb65b1b3ec7cd279663c80
Preston Church <pchurch2@uccs.edu>
2019-12-02 14:20:25 -0700

    Runtime user creds and refactoring

--- apk_download.py
+++ apk_download.py
@@ -2,20 +2,18 @@
 import sys
 import os
 import re
+from getpass import getpass
 from Naked.toolshed.shell import execute_js, muterun_js
 from gpapi.googleplay import GooglePlayAPI, RequestError
+from datetime import datetime
 
 
 # GooglePlay-Scraper Variables
 collection_type = "TOP_FREE"
-app_per_category = "3"
+app_per_category = "1"
 apk_initial_list = []
 apk_download_list = []
-
-
-# Login Variables
-#gsfId = 
-#authSubToken = 
+category_apk_list = {} #####
 
 
 # Google Server Variables
@@ -23,8 +21,19 @@ LOCALE = "us_US"
 TIMEZONE = "America/Chicago"
 server = GooglePlayAPI("us_US","America/Denver")
 
+# Directory Variables
+timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+apk_dir = "./apks_" + timestamp
+
+def get_credentials():
+
+    gsfId = getpass("GSFID: ")
+    authSubToken = getpass("AUTHSUBTOKEN: ")
+
+    return gsfId, authSubToken
 
 def remove_duplicate_ids(id_list):
+    
     return list(dict.fromkeys(id_list))
 
 
@@ -45,15 +54,29 @@ def get_app_id(category_type):
         category_list = re.findall(r"appId: '.*?'", str(input_apps))
         download = re.findall(r"'(.*?)'", str(category_list))
 
+	    # Initialize category in list
+        #category_apk_list.setdefault(str(category_type), [])  #####
+        #testlist = []
+
         # Grab non-empty apk_ids
         for app in download:
             if app != "":
                 print(app)
                 apk_initial_list.append(app)
+                #testlist.append(app)
+                #category_apk_list(str(category_type),[]).append(str(app))  #####
+                
+        #for app_in_category in testlist:
+            #category_apk_list.setdefault(str(category_list), []).append(str(app_in_category))
+        #return testlist
 
     else:
         sys.stderr.write(get_apps.stderr)
         #return
+
+    #Test
+    #for item in category_apk_list:	#####
+        #print("Testing: " + category_apk_list)  #####
 
 
 def generate_apk_list():
@@ -71,9 +94,25 @@ def generate_apk_list():
 
         print("\nGenerating app_id list.  Please wait...\n")
 
+      # Initialize category in list
+        #category_apk_list.setdefault(str(category_type), [])  #####
+
         for title in categories:
+            
             print("\n" + title.center(50, '-') + "\n")
             get_app_id(title)
+            
+            # Create Directories
+            try:
+
+                os.makedirs(apk_dir + "/" + title)
+                print("Directory created for: " + title)
+
+            except FileExistsError:
+
+                # Directory Already Exists
+                pass
+                print("Directory already exists for: " + title)
 
         # Remove any dupliacte app_id entries and sort in ascending order
         apk_download_list = remove_duplicate_ids(apk_initial_list)
@@ -93,8 +132,9 @@ def generate_apk_list():
 
 def server_login():
 
-    print("\nLogging in with ac2dm token and gsfId.\n")
-    server.login(None, None, gsfId, authSubToken)
+    print("\nLogging in with GSFID and SubAuthToken (ac2dm).\n")
+    gsfId, authSubToken = get_credentials() 
+    server.login(None, None, int(gsfId), authSubToken)
 
 
 def download_apks(download_apk):
@@ -103,18 +143,29 @@ def download_apks(download_apk):
     server.log(app_id)
     
     print("\nDownloading {}...".format(app_id))
+   
+    try:
+
+        app = server.download(app_id)
+
+        with open(app_id + ".apk", "wb") as apk_file:
+            for app in app.get("file").get("data"):
+                apk_file.write(app)
+                #apk_file.write(apk_dir + "/" + app)
+            print("Success")
     
-    app = server.download(app_id)
-    
-    with open(app_id + ".apk", "wb") as apk_file:
-        for chunk in app.get("file").get("data"):
-            apk_file.write(chunk)
-        print("Success")
+    except Exception:        
+        
+        pass
+        print("Failed")
 
 def main():
 
-    library = generate_apk_list()
     server_login()
+    library = generate_apk_list()
+	
+    #print("Categories and Apps\n\n")
+    #print(category_apk_list)
 
     # Download APKs
     print("\nDownloading APKs.  Please wait...\n")
